ebf0305ac0233a6c7cecc57d1b973414d3faf3be
Administrator <Administrator@Asus>
2016-11-06 23:30:33 +0800

    11/06 Committed Chater 09 test.

---
+++ src/IZ0_803/Q9_33.java
@@ -0,0 +1,20 @@
+package IZ0_803;
+
+public class Q9_33 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Byte x=2;
+		
+		switch (x) {
+		case 1:
+			System.out.println("This is one");
+			break;
+		case 2:
+			System.out.println("This is two");
+			break;			
+		}
+	}
+
+}
